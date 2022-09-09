9158b20e8244cd5fd743fbf415e6cca8db6d1d2a
riikkapurho <113058359+riikkapurho@users.noreply.github.com>
2022-09-09 22:31:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const addMyButton = document.getElementById("my-button");
 
 addMyButton.addEventListener("click", function () {
   console.log("Hello world");
-  /*document.getElementById("hello").textContent = "My notebook";*/
+  document.getElementById("hello").textContent = "My notebook";
 });
