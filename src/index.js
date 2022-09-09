61e2f6060696ed0cf8720e73d143f6dbd8e11064
riikkapurho <113058359+riikkapurho@users.noreply.github.com>
2022-09-09 22:25:49 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const addMyButton = document.getElementById("my-button");
+
+addMyButton.addEventListener("click", function () {
+  console.log("Hello world");
+  /*document.getElementById("hello").textContent = "My notebook";*/
+});
